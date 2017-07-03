9e2254aab4611ab91a624511281bd922233e27fd
Challen <273239067@qq.com>
2017-07-03 20:11:59 +0800

    FB

---
+++ src/com/java/Mll.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Mll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		System.out.println("s="+s);
+				
+	}
+
+}
---
+++ src/com/java/testJava.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class testJava {
+
+	
+	   public static void main(String args[]){double d1=-0.5;
+	   System.out.println("Ceil d1="+Math.ceil(d1));
+	   System.out.println("floor d1="+Math.floor(d1));}
+}
+
